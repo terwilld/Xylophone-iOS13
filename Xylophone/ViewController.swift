f6254a541908eb4620ae964de4fe883c94e83047
David Terwilliger <terwilld@gmail.com>
2022-04-14 15:19:22 -0400

    That was dumb.  Found the exact answer on stack overflow first try. Lots of people have tried that. The async thing is actually pretty cool though.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -18,7 +18,12 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         //print(sender.currentTitle)
-       
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
         
         playSound(note: sender.currentTitle! )
     }
